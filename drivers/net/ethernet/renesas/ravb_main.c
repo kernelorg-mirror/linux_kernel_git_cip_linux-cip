13eadcc992d4f3be88aada957b00679d5c15be83
Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>
2024-05-29 11:14:27 +0300

    net: ravb: Move the update of ndev->features to ravb_set_features()
    commit 7bddccc9911cdff377e16b9a5a386721279b4438 upstream.
    
    Commit c2da9408579d ("ravb: Add Rx checksum offload support for GbEth")
    introduced support for setting GbEth features. With this the IP-specific
    features update functions update the ndev->features individually.
    
    Next commits add runtime PM support for the ravb driver. The runtime PM
    implementation will enable/disable the IP clocks on
    the ravb_open()/ravb_close() functions. Accessing the IP registers with
    clocks disabled blocks the system.
    
    The ravb_set_features() function could be executed when the Ethernet
    interface is closed so we need to ensure we don't access IP registers while
    the interface is down when runtime PM support will be in place.
    
    For these, move the update of ndev->features to ravb_set_features(). In
    this way we update the ndev->features only when the IP-specific features
    set function returns success and we can avoid code duplication when
    introducing runtime PM registers protection.
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>
    Reviewed-by: Sergey Shtylyov <s.shtylyov@omp.ru>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>
    Reviewed-by: Nobuhiro Iwamatsu <nobuhiro1.iwamatsu@toshiba.co.jp>
    Signed-off-by: Pavel Machek <pavel@denx.de>

--- drivers/net/ethernet/renesas/ravb_main.c
+++ drivers/net/ethernet/renesas/ravb_main.c
@@ -2581,7 +2581,6 @@ static int ravb_set_features_gbeth(struct net_device *ndev,
 			goto done;
 	}
 
-	ndev->features = features;
 done:
 	spin_unlock_irqrestore(&priv->lock, flags);
 
@@ -2596,8 +2595,6 @@ static int ravb_set_features_rcar(struct net_device *ndev,
 	if (changed & NETIF_F_RXCSUM)
 		ravb_set_rx_csum(ndev, features & NETIF_F_RXCSUM);
 
-	ndev->features = features;
-
 	return 0;
 }
 
@@ -2606,8 +2603,15 @@ static int ravb_set_features(struct net_device *ndev,
 {
 	struct ravb_private *priv = netdev_priv(ndev);
 	const struct ravb_hw_info *info = priv->info;
+	int ret;
 
-	return info->set_feature(ndev, features);
+	ret = info->set_feature(ndev, features);
+	if (ret)
+		return ret;
+
+	ndev->features = features;
+
+	return 0;
 }
 
 static const struct net_device_ops ravb_netdev_ops = {
