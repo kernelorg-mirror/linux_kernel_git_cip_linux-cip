246a252491666384ca1eb7475aadd247623881a4
Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>
2026-01-15 10:34:43 +0200

    clk: renesas: rzg2l: Postpone updating priv->clks[]
    commit 2f96afdffad4ef74e3c511207058c41c54a2d014 upstream.
    
    Since the sibling data is filled after the priv->clks[] array entry is
    populated, the first clock that is probed and has a sibling will
    temporarily behave as its own sibling until its actual sibling is
    populated. To avoid any issues, postpone updating priv->clks[] until after
    the sibling is populated.
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Link: https://lore.kernel.org/20250514090415.4098534-2-claudiu.beznea.uj@bp.renesas.com
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    [claudiu.beznea: fixed conflict by keeping code from v6.1 CIP]
    Signed-off-by: Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>
    Reviewed-by: Nobuhiro Iwamatsu <nobuhiro.iwamatsu.x90@mail.toshiba>
    Signed-off-by: Pavel Machek <pavel@nabladev.com>

--- drivers/clk/renesas/rzg2l-cpg.c
+++ drivers/clk/renesas/rzg2l-cpg.c
@@ -1401,9 +1401,6 @@ rzg2l_cpg_register_mod_clk(const struct rzg2l_mod_clk *mod,
 	if (IS_ERR(clk))
 		goto fail;
 
-	dev_dbg(dev, "Module clock %pC at %lu Hz\n", clk, clk_get_rate(clk));
-	priv->clks[id] = clk;
-
 	if (mod->is_coupled) {
 		struct mstp_clock *sibling;
 
@@ -1414,6 +1411,9 @@ rzg2l_cpg_register_mod_clk(const struct rzg2l_mod_clk *mod,
 			sibling->sibling = clock;
 		}
 	}
+
+	dev_dbg(dev, "Module clock %pC at %lu Hz\n", clk, clk_get_rate(clk));
+	priv->clks[id] = clk;
 
 	return;
 
