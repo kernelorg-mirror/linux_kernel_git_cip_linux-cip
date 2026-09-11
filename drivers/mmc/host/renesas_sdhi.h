fd789f55e24ad2322eb1368b13ae983da0fc9ba7
Wolfram Sang <wsa+renesas@sang-engineering.com>
2026-09-11 17:01:48 +0300

    mmc: renesas_sdhi: improve naming of DMA struct
    commit 7f3ea248cd1b2ea9b49f6f3523250bc8e622dd5e upstream.
    
    Commit 058db2868cd8 ("mmc: tmio, renesas_sdhi: move struct tmio_mmc_dma
    to renesas_sdhi.h") is correct. The DMA struct should be prefixed with
    'renesas_sdhi' to avoid confusion about is namespace. Fix some
    indentation while here.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Tested-by: Duy Nguyen <duy.nguyen.rh@renesas.com>
    Tested-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Link: https://lore.kernel.org/r/20221006190452.5316-3-wsa+renesas@sang-engineering.com
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>
    Signed-off-by: Nobuhiro Iwamatsu <nobuhiro.iwamatsu.x90@mail.toshiba>
    Reviewed-by: Pavel Machek <pavel@nabladev.com>

--- drivers/mmc/host/renesas_sdhi.h
+++ drivers/mmc/host/renesas_sdhi.h
@@ -50,19 +50,19 @@ struct renesas_sdhi_of_data_with_quirks {
 	const struct renesas_sdhi_quirks *quirks;
 };
 
-struct tmio_mmc_dma {
+struct renesas_sdhi_dma {
 	enum dma_slave_buswidth dma_buswidth;
 	bool (*filter)(struct dma_chan *chan, void *arg);
 	void (*enable)(struct tmio_mmc_host *host, bool enable);
-	struct completion	dma_dataend;
-	struct tasklet_struct	dma_complete;
+	struct completion dma_dataend;
+	struct tasklet_struct dma_complete;
 };
 
 struct renesas_sdhi {
 	struct clk *clk;
 	struct clk *clk_cd;
 	struct tmio_mmc_data mmc_data;
-	struct tmio_mmc_dma dma_priv;
+	struct renesas_sdhi_dma dma_priv;
 	const struct renesas_sdhi_quirks *quirks;
 	struct pinctrl *pinctrl;
 	struct pinctrl_state *pins_default, *pins_uhs;
